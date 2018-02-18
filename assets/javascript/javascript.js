fad1009b7a2937a6042a315d01d1a9f2c3312652
Patrick East <Patrick@Patricks-MacBook-Pro.local>
2018-02-18 12:15:52 -0700

    Getting started on JS.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -29,3 +29,17 @@ var computerChoice = [
     "Uncompahgre Peak", 
 
 ]
+
+given correct guesses Array["a", "f"]
+
+given current guess
+"r"
+Loop through each letter of the WaveShaperNodeadd undersccore Headersloop through each correct SVGFEGaussianBlurElement if letter is same as current letter of word then replace with letter
+
+given a word to check against
+"fair"
+
+given wa word to check against"Fair"
+
+then show a string with correct WEBGL_compressed_texture_s3tc
+"fa_r"
